f8e04c83a4d802e19dd8b9e8a753a1466290fc7f
Jihoon <grcosmos@gmail.com>
2020-03-13 14:00:07 +0100

    Have some exemple executions

--- DLE_run_scaleup.R
+++ DLE_run_scaleup.R
@@ -1,6 +1,8 @@
 library(countrycode)
 library(tidyverse)
 
+source('DLE_integration_data_structure.R')
+
 # Example for health dimension
 countries <- codelist$iso3c[!is.na(codelist$iso3c)]
 group <- c('adult', 'child')
@@ -9,7 +11,19 @@ idx <- expand.grid(iso=countries, grp=group) %>% arrange(iso, grp)
 thres <- data.frame(idx, val=2000)
 gap <- data.frame(idx, val=2000)
 
+# Initiate an inherited, specific dimension object
 DLE.Health <- DLE.dimension.health(indicator = 'kcal/day', thres = thres)
 
+# Initiate a general dimension object
+DLE.A <- DLE.dimension(indicator = 'xxx', thres = thres)
+
+# Do whatever necessary for the initial setup
+DLE.Health$DeriveThreshold()
 DLE.Health$IdentifyGap()
-DLE.Health
+
+# Build a list of dimensions
+dim.list <- list(health = DLE.Health, A = DLE.A)
+
+# Initiate a scenario object
+DLE.BAU <- DLE.scenario(scenario = "BAU", dims = dim.list)
+DLE.BAU$SetupRollout()
